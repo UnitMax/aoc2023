937f2749ab42e3794a4a41159bd1887b8ebd272c
UnitMax <105129888+UnitMax@users.noreply.github.com>
2023-12-03 18:41:50 +0100

    initial commit

---
+++ src/main/java/unitmax/App.java
@@ -0,0 +1,13 @@
+package unitmax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
